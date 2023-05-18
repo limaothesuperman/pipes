d0e4eeaea4fe0d7252fcf26111fe18acbb0bc583
Mao Li <mli94@stevens.edu>
2023-05-18 16:42:58 -0500

    added pipes feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+
+  name = '';
+  date: string;
+  amount: number;
+
+  onNameChange(value: string) {
+    this.name = value;
+  }
+  onDateChange(value: string) {
+    this.date = value;
+  }
+  onAmountChange(value: string) {
+    this.amount = parseFloat(value);
+  }
+
 }
