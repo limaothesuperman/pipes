9809700c5290eeb5188b9cec2c5042808577cc26
Mao Li <mli94@stevens.edu>
2023-05-19 14:40:46 -0500

    added custom pipes and finished application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,6 +10,23 @@ export class AppComponent {
   name = '';
   date: string;
   amount: number;
+  height: number;
+  miles: number;
+
+  car = {
+    make: 'BMW',
+    model: 'X5',
+    year: 2019
+  }
+
+  onMilesChange(value: string) {
+    this.miles = parseFloat(value);
+
+  }
+
+  onHeightChange(value: string) {
+    this.height = parseFloat(value);
+  }
 
   onNameChange(value: string) {
     this.name = value;
